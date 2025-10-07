e091271bade4c2b5ff7ae1b4ce6d807f0b6ea899
parth5012 <parthchawla5012@GMAIL.COM>
2025-10-07 15:20:17 +0530

    Added project details and contacting page

--- app.py
+++ app.py
@@ -25,6 +25,21 @@ def resume():
 def about():
     return render_template('about.html')
 
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('project/{id}')
+def project_details(id):
+
+    # extracting project details from db
+    return render_template('project_details.html')
+
+
+
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
